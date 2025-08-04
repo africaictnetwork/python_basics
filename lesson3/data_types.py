1818fed33aab72f34f727e654d3510f424bfab8c
otundoMjoseph <josephmainaotundo@gmail.com>
2025-08-04 13:45:38 +0300

    updated lesson 3 partially

---
+++ lesson3/data_types.py
@@ -0,0 +1,47 @@
+# String data type 
+
+#literal Assignment
+# first = "Joe"
+
+# print(type(first))
+# print(type(first) == str)
+# print(isinstance(first, str))
+
+
+#Constructor assignment
+# pizza = str("Pepporoni")
+# print(type(pizza))
+# print(type(pizza)==str)
+# print(isinstance(pizza, str))
+
+# Concatenation
+
+first_name = "Joseph"
+second_name = "Otundo"
+full_Name = first_name + " " + second_name
+full_Name += "!!"
+print(full_Name)
+
+#Python type Casting
+decade = str(1980)
+print(type(decade))
+print(isinstance(decade, str))
+
+statement = "I like music from the " + decade + "s"
+print(statement)
+
+#Multilines
+multilines = '''
+
+hey how are you 
+        I was just checking in
+
+    all good?😂😂❤️❤️😍
+
+                '''
+print(multilines)
+
+#escaping special characters
+
+
+
