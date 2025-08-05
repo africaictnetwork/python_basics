96721db3710ddc2815f18115e2e0c35da934ea70
otundoMjoseph <josephmainaotundo@gmail.com>
2025-08-05 22:07:37 +0300

    created a menu project

--- lesson3/data_types.py
+++ lesson3/data_types.py
@@ -16,32 +16,51 @@
 
 # Concatenation
 
-first_name = "Joseph"
-second_name = "Otundo"
-full_Name = first_name + " " + second_name
-full_Name += "!!"
-print(full_Name)
+# first_name = "Joseph"
+# second_name = "Otundo"
+# full_Name = first_name + " " + second_name
+# full_Name += "!!"
+# print(full_Name)
 
-#Python type Casting
-decade = str(1980)
-print(type(decade))
-print(isinstance(decade, str))
+# #Python type Casting
+# decade = str(1980)
+# print(type(decade))
+# print(isinstance(decade, str))
 
-statement = "I like music from the " + decade + "s"
-print(statement)
+# statement = "I like music from the " + decade + "s"
+# print(statement)
 
 #Multilines
-multilines = '''
+# multilines = '''
 
-hey how are you 
-        I was just checking in
+# hey how are you 
+#         I was just checking in
 
-    all good?😂😂❤️❤️😍
+#     all good?😂😂❤️❤️😍
 
-                '''
-print(multilines)
+#                 '''
+# print(multilines)
 
 #escaping special characters
+
+sentence = "I\'m back at work! \t Hey! \n\n where\'s this at \\located"
+
+#string methods
+# print(sentence.lower())
+# print(sentence.upper())
+# print(sentence )
+# print(sentence.title())
+# print(sentence.replace("work", "Job"))
+sentence += "                                                                   "
+
+print(len(sentence))
+print(len(sentence.lstrip()))
+print(len(sentence.rstrip()))
+print(len(sentence.strip()))
+
+
+
+
 
 
 
---
+++ menu.py
@@ -0,0 +1,59 @@
+import math
+title = "menu".upper()
+print(title.center(20, "="))
+print("Coffee".ljust(16, "." )+ "1$".rjust(4))
+print("Muffin".ljust(16, "." )+ "2$".rjust(4))
+print("Ice-cream".ljust(16, "." )+ "3$".rjust(4))
+
+
+#indexing menu
+
+print(title[0])
+
+
+# some methods return boolean data
+print(title.startswith("M"))
+print(title.endswith("U"))
+
+#boolean data
+myValue = True
+x = bool(False)
+print(type(x))
+print(isinstance(myValue, bool))
+
+# numeric data type 
+# int types
+
+price = 100
+base_price = int(150)
+print(type(base_price))
+print(isinstance(base_price, int))
+
+#float type 
+gpa = 3.8
+y = float(1.8)
+
+print(type(gpa))
+
+#complex type
+
+comp_value = 5 + 3j
+print(comp_value)
+print(type(comp_value))
+print(comp_value.real)
+print(comp_value.imag)
+
+#built in functions for numbers
+abs(gpa)
+print(abs(gpa * -1))
+print(round(gpa))
+print(round(gpa, 0))
+
+print(math.pi)
+print(math.sqrt(64))
+print(math.ceil(gpa))
+print(math.floor(gpa))
+
+
+
+
